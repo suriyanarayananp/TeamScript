eaaa6ed65de851e5b7453f7086d9b31f775b54dc
kalyanaraman <kalyanaraman.radhakannan@meritgroup.co.uk>
2015-02-27 09:11:55 +0530

    Updated Zara-ES  List Robot
    Updated Zara-ES  List Robot

--- ListRobots/Zara-ES--List.pl
+++ ListRobots/Zara-ES--List.pl
@@ -104,10 +104,10 @@ sub Product_Collection
 	my $menu4=shift;
 	
 	my $cat_id;
-	 if($category_url=~m/\-?\s*c\s*(\d+)\s*\.\s*html/is)
+	if($category_url=~m/\-?\s*c\s*(\d+)\s*\.\s*html/is)
 	{
 		$cat_id=$1;
-	}
+	}	
 	
 	my $Product_list_content = $utilityobject->Lwp_Get($category_url);
 	
@@ -116,7 +116,7 @@ sub Product_Collection
 	
 	my $color_url='http://www.zara.com/webapp/wcs/stores/servlet/CategoryFilterJSON?categoryId='.$cat_id.'&langId=-1&storeId=10706&filterCode=DYNAMIC&ajaxCall=true';
 	# Formation of color url for the produts under respective category(In filter). 
-
+	
 	my $char_Cont = $utilityobject->Lwp_Get($char_url);
 	my $color_Cont = $utilityobject->Lwp_Get($color_url);
 			
@@ -129,7 +129,7 @@ sub Product_Collection
 		$product_url = $1;
 		
 		# Pattern match to get the product id to remove duplicate product urls.
-		$prod_id=$1 if($product_url=~m/p\s*(\d+)\s*\.\s*html/is);
+		$prod_id=$1 if($product_url=~m/p\s*(\d+)\s*\.\s*html/is);		
 		
 		# Insert Product values.
 		if($hash_id{$prod_id} eq  '')
@@ -143,7 +143,7 @@ sub Product_Collection
 			# Assigning objectkey of already existed product's Objectkey and skip the url if exist and save the new product's tag information along with already existed product url's tag information.
 			$product_object_key=$hash_id{$prod_id};
 		}
-		
+				
 		# Save the tag information of "Menu 1" if non-empty.
 		$dbobject->SaveTag('Menu_1',$menu1,$product_object_key,$robotname,$Retailer_Random_String) if($menu1 ne '');  
 		
@@ -158,7 +158,7 @@ sub Product_Collection
 		
 		# Committing the transaction.
 		$dbobject->commit();
-		
+
 		# Taking characteristics and color tag. 
 		&charactertag($char_Cont,$prod_id,$product_object_key);# Function call to get characteristics tag information for the corresponding product.
 		&charactertag($color_Cont,$prod_id,$product_object_key);# Function call to get colour tag information for the corresponding product. 
@@ -170,6 +170,7 @@ sub charactertag() # Function definition to get characteristics and colour tag i
 	my $Jcont1=shift;
 	my $prod_id1=shift;
 	my $product_object_key1=shift;
+
 	while($Jcont1=~m/{\s*\"\s*values\s*\"([\w\W]*?)\"\s*type\s*\"\s*\:\s*\"\s*([^>]*?)\s*\"\s*}/igs)  # Characteristics,Colour and quality block (In Filter in RHS).
 	{
 		my $Type1_blk=$1;
@@ -184,6 +185,7 @@ sub charactertag() # Function definition to get characteristics and colour tag i
 			my $name1;
 			
 			$name1=$1 if($Type_blk11=~m/\s*\"\s*name\s*\"\s*\:\s*\"([^\'\"]*?)\s*\"/is); # Getting tag name Eg. Characteristics or color.
+
 			# Pattern match to get the sku id's(Product id's) block from the main block.
 			if($Type_blk11=~m/\"\s*skus\s*\"\s*\:([\w\W]*?\"\])/is)   
 			{
@@ -193,11 +195,16 @@ sub charactertag() # Function definition to get characteristics and colour tag i
 				while($Skus_id_blk1=~m/(?:\"|\')\s*([^\'\"]*?)\s*(?:\"|\')/igs)
 				{
 					my $Skuid1=$1;
+					
 					# Pattern match to get the tag information if product id in product url and sku id from the block is same.
-					if(($Skuid1 eq $prod_id1)||($Skuid1=~m/$prod_id1/))
+
+					$prod_id1=~s/\s+//igs;
+					$Skuid1=~s/\s+//igs;
+					if($Skuid1 == $prod_id1)
 					{
 						$Type1=~s/features/characteristics/igs;
 						$Type1=~s/quality/qualities/igs;
+
 						# Save the tag information into tag table.
 						$dbobject->SaveTag($Type1,$name1,$product_object_key1,$robotname,$Retailer_Random_String);
 						# Committing the transaction.
